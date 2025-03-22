4322bde2bcb6d7d74ac5cab54480789e81ba866b
Dexcop <ardellejodynathaniel@gmail.com>
2025-03-22 20:59:15 +0700

    added background color and completed FoodListNotification

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,21 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./component/Navbar";
+import NutriWiseBanner from "./component/FoodListNotif";
 
 function App() {
   return (
-<div className="App">
+    <div className="App">
       <Navbar />
+      <NutriWiseBanner
+        username="Hutao"
+        foodItems={[
+          // { name: "Milk", expiryDate: new Date("2025-03-23") },
+          { name: "Eggs", expiryDate: new Date("2023-08-20") }
+        ]}
+        onAddFood={() => console.log("Add food")}
+        onExpiryPress={() => console.log("View details")}
+      />
     </div>
   );
 }
