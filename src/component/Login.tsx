77397d2c52bcec214aa00e7a42138a2423d62814
Kenneth Andrew Lukita <kennethandrew1602@gmail.com>
2025-04-25 20:27:41 +0700

    tambah komen

--- src/component/Login.tsx
+++ src/component/Login.tsx
@@ -2,5 +2,8 @@ import React from "react";
 
 export const Login = () => {
   return (<div>Loginoeproeporpa</div>);
-  
+  //1. rajin-rajin git pull
+  //2. kalo udah commit, git pull dulu baru pull request
+  //3. kalo ada error jangan pull request dulu
+  //4. setelah commit gui, sync change
 }; 
