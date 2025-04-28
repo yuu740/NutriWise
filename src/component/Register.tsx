1f6db0db0b4cf41f48b27a7bc3e12976e055eb21
Kenneth Andrew Lukita <kennethandrew1602@gmail.com>
2025-04-28 09:59:37 +0700

    register selesai

--- src/component/Register.tsx
+++ src/component/Register.tsx
@@ -1,10 +1,5 @@
 import React from "react";
 
-  //1. rajin-rajin git pull
-  //2. kalo udah commit + sync change, git pull dulu baru pull request
-  //3. kalo ada error jangan pull request dulu
-  //4. setelah commit gui, sync change (git push tapi di source control)
-
 export const Register = () => {
   return(
     <div style={styles.page}>
