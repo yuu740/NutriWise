558832ef45d484e6daa3788286de377fe9acd207
Revel <revelcahyadi2004@gmail.com>
2025-05-16 17:01:28 +0700

    style : refactor code from inline style

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ApiService } from "../constant/ApiService";
 import { toast, ToastContainer } from "react-toastify";
+import "../styles/login.css";
 
 //1. rajin-rajin git pull
 //2. kalo udah commit + sync change, git pull dulu baru pull request
@@ -11,8 +12,7 @@ import { toast, ToastContainer } from "react-toastify";
 export const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [message, setMessage] = useState("");
-  
+
   const navigate = useNavigate();
 
   const handleLogin = async () => {
@@ -30,31 +30,31 @@ export const Login = () => {
   };
   return (
     <>
-    <div style={styles.page}>
-      <h1 style={styles.title}>NutriWise</h1>
-      <div style={styles.formContainer}>
-        <label style={styles.email}>Email</label>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          style={styles.inputemail}
-        />
+      <div className="login-page">
+        <h1 className="login-test">NutriWise</h1>
+        <div className="login-form">
+          <label className="login-label email">Email</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="login-input"
+          />
 
-        <label style={styles.password}>Password</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          style={styles.inputpassword}
-        />
+          <label className="login-label password">Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="login-input"
+          />
 
-        <button onClick={handleLogin} style={styles.button}>
-          Join
-        </button>
+          <button onClick={handleLogin} className="login-button">
+            Join
+          </button>
+        </div>
       </div>
-    </div>
-    <ToastContainer
+      <ToastContainer
         position="top-right"
         autoClose={3000}
         hideProgressBar={false}
@@ -66,93 +66,6 @@ export const Login = () => {
         pauseOnHover
       />
     </>
-    
   );
 };
 
-const styles: { [key: string]: React.CSSProperties } = {
-  page: {
-    height: "100vh",
-    backgroundColor: "#fdf0b7",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  title: {
-    color: "#cc6600",
-    fontFamily: '"Playfair Display SC", serif',
-    fontWeight: "bold",
-    marginBottom: "20px",
-  },
-
-  formContainer: {
-    backgroundColor: "#FFB35C",
-    padding: "40px",
-    display: "flex",
-    flexDirection: "column",
-    gap: "15px",
-    minWidth: "300px",
-    height: "300px",
-    borderRadius: "15px",
-    position: "relative",
-  },
-
-  email: {
-    fontFamily: '"Playfair Display", serif',
-    position: "relative",
-    fontSize: "20px",
-    bottom: "10px",
-    textAlign: "left",
-    left: "5px",
-    marginBottom: "-10px",
-  },
-
-  password: {
-    fontFamily: '"Playfair Display", serif',
-    marginBottom: "-5px",
-    position: "relative",
-    fontSize: "20px",
-    textAlign: "left",
-    left: "5px",
-  },
-
-  inputemail: {
-    padding: "10px",
-    borderRadius: "15px",
-    border: "none",
-    backgroundColor: "#FFB1B1",
-    fontSize: "16px",
-    height: "27px",
-  },
-
-  inputpassword: {
-    padding: "10px",
-    borderRadius: "15px",
-    border: "none",
-    backgroundColor: "#FFB1B1",
-    height: "27px",
-    fontSize: "16px",
-  },
-
-  button: {
-    marginTop: "78px",
-    marginLeft: "200px",
-    padding: "10px",
-    backgroundColor: "#F7E255",
-    border: "none",
-    borderRadius: "15px",
-    cursor: "pointer",
-    width: "64px",
-    fontFamily: '"Playfair Display", serif',
-    height: "30px",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    textAlign: "center",
-    position: "absolute",
-    bottom: "14px",
-    right: "24px",
-  },
-};
