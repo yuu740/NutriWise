079dcbd578fc689592c464e78480e91a76d6612b
Revel <revelcahyadi2004@gmail.com>
2025-05-20 10:22:32 +0700

    init (FoodList) : make interface for Foodlist,addfood,addfoodreqdto

--- src/interface/Foodlist.ts
+++ src/interface/Foodlist.ts
@@ -1,6 +1,20 @@
 export interface Foodlist {
-    foodName: string;
+    food_name: string;
     quantity: number;
-    expiryDate: Date;
-    isDeleted: boolean;
+    expiry_date: Date;
+    is_deleted: boolean;
+}
+
+export interface AddFood {
+    food_name?: string;
+    quantity?: number;
+    expiry_date?: Date;
+}
+
+export interface AddFoodReqDTO {
+    username: string;
+    food_name: string;
+    quantity: number;
+    expiry_date: Date;
+
 }
