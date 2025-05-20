d76ff568cd3c95c894654a99dd71490609665c84
Revel <revelcahyadi2004@gmail.com>
2025-05-20 10:23:05 +0700

    fix (ModalAdd) : fix the value for submit passing into json logic

--- src/components/ModalAdd.tsx
+++ src/components/ModalAdd.tsx
@@ -1,26 +1,28 @@
 import React, { FC } from "react";
 import { Form } from "react-router-dom";
 import "../styles/modaladd.css";
+import { AddFood } from "../interface/Foodlist";
 
 interface ModalAddProps {
   onClose: () => void;
   onSubmit: () => void;
+  addedFoodList?: AddFood;
 }
 
-export const ModalAdd: FC<ModalAddProps> = ({ onClose, onSubmit }) => {
+export const ModalAdd: FC<ModalAddProps> = ({ onClose, onSubmit, addedFoodList}) => {
   return (
     <div className="modal-overlay">
       <div className="modal-content">
         <label>Food Type</label>
-        <input type="text" className="modal-input" />
+        <input type="text" className="modal-input" value={addedFoodList?.food_name || ""}/>
 
         <label>Quantity</label>
-        <input type="number" className="modal-input" />
+        <input type="number" className="modal-input" value={addedFoodList?.quantity ||  0} />
 
         <label>Expiry Date</label>
         <input type="date" className="modal-input" />
         <div className="modal-actions">
-          <button className="modal-cancel-btn" onClick={onClose}>
+          <button className="modal-cancel-btn" value={addedFoodList?.expiry_date ? String(addedFoodList.expiry_date) : undefined} onClick={onClose}>
             Cancel
           </button>
           <button className="modal-add-btn" onClick={onSubmit}>
