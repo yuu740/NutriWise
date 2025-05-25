87e1469a9ee65fb2cb0ded7a4a29165ede33069c
Revel <revelcahyadi2004@gmail.com>
2025-05-25 18:22:12 +0700

    feat (interface) : make interface for recipe recom

---
+++ src/interface/Recipe.ts
@@ -0,0 +1,15 @@
+interface RecipeResDTO {
+  available_ingredients: string[];
+  ingredient_details: string[]; 
+  recipe_steps: string[];
+  recipe_title: string;
+  total_ingredients: number;
+  unavailable_ingredients: string[];
+}
+interface Recipe {
+  recipe_name: string;
+  ingredients: { name: string; has: boolean; }[]; 
+  availability_percentage: number;
+  missing_all_ingredients: boolean;
+  can_make_now: boolean;
+}
